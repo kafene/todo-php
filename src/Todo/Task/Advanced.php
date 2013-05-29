fb97b42f7f95f1e9e70921bc41540987b7a21112
Sanpi <sanpi@homecomputing.fr>
2013-05-29 21:49:35 +0200

    Parse comment

--- src/Todo/Task/Advanced.php
+++ src/Todo/Task/Advanced.php
@@ -4,4 +4,28 @@ namespace Todo\Task;
 
 class Advanced extends Simple
 {
+    public $comment;
+
+    public function load($txt)
+    {
+        parent::load($txt);
+        $txt = $this->description;
+
+        if (preg_match('#(?P<txt>.*?) ?=> ?(?<comment>.*)$#', $txt, $matches) === 1) {
+            $this->comment = $matches['comment'];
+            $txt = $matches['txt'];
+        }
+
+        $this->description = $txt;
+    }
+
+    public function __toString()
+    {
+        $txt = parent::__toString();
+
+        if (!is_null($this->comment)) {
+            $txt .= " => {$this->comment}";
+        }
+        return $txt;
+    }
 }
--- tests/Units/Todo/Task/Advanced.php
+++ tests/Units/Todo/Task/Advanced.php
@@ -9,4 +9,19 @@ class Advanced extends \atoum
         $this->object(new \Todo\Task\Advanced)
             ->isInstanceOf('\Todo\Task\Advanced');
     }
+
+    public function testParseComment()
+    {
+        $txt = '(A) Crack the Da Vinci Code. => A comment';
+        $task = new \Todo\Task\Advanced($txt);
+
+        $this->castToString($task)
+            ->isEqualTo($txt);
+
+        $this->string($task->description)
+            ->isEqualTo('Crack the Da Vinci Code.');
+
+        $this->string($task->comment)
+            ->isEqualTo('A comment');
+    }
 }
