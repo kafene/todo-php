d4578273e003d1d26a727321b3dca34859c26c06
Sanpi <sanpi@homecomputing.fr>
2013-06-01 21:27:37 +0200

    Use atoum bootstrap file

--- .atoum.php
+++ .atoum.php
@@ -1,4 +1,3 @@
 <?php
 
-$runner->setBootstrapFile(__DIR__ . '/tests/Units/bootstrap.php')
-    ->addTestsFromDirectory(__DIR__ . '/tests/Units');
+$runner->addTestsFromDirectory(__DIR__ . '/tests/Units');
